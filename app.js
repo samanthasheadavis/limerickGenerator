cadb0b0f2b3789e4e551cf3ad2f0792ac053f36c
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-26 14:10:45 -0500

    quick fix to the getNouns function

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ angular.module('limerickApp', []).controller("limerickController", function($sco
 
   self.getNouns = function(wordsArray) {
     var nouns = [];
-    var rand = Math.floor(Math.random()*wordsArray.length);
     for (count=0; count<wordsArray.length-1; count++) {
       if (wordsArray[count].tags[0] === 'n') {
         nouns.push(wordsArray[count].word);
       }
     }
+    var rand = Math.floor(Math.random()*nouns.length);
     self.noun = nouns[rand];
     console.log(self.noun);
   };
