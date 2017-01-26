5799e6b02475601cb6037bb8b6c407596a84c1bb
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-26 14:07:01 -0500

    wrote function to find random rhyming noun

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 angular.module('limerickApp', []).controller("limerickController", function($scope, $http, RequestService){
-var rhyme = 'home';
-  this.getRhymes = function() {
-    RequestService.getRhymes(rhyme, function(response) {
-      console.log(response.data);
+  var self = this;
+  this.isNoun = function(noun) {
+    return noun === "n";
+  };
+
+  this.getRhymes = function(word) {
+    RequestService.getRhymes(word, function(response) {
+      $scope.rhymes = response.data;
+      self.getNouns($scope.rhymes);
     });
   };
 
-  this.getRhymes();
+  self.getNouns = function(wordsArray) {
+    var nouns = [];
+    var rand = Math.floor(Math.random()*wordsArray.length);
+    for (count=0; count<wordsArray.length-1; count++) {
+      if (wordsArray[count].tags[0] === 'n') {
+        nouns.push(wordsArray[count].word);
+      }
+    }
+    self.noun = nouns[rand];
+    console.log(self.noun);
+  };
 });
