abe5f0ee76c8bb4396edb10e2ba5b592619a64b5
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-27 14:41:27 -0500

    built out layout with css and added sliding animation

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
-angular.module('limerickApp', []).controller("limerickController", function($scope, $http, RequestService){
-  var self = this;
-  self.show=false;
-  this.isNoun = function(noun) {
-    return noun === "n";
-  };
+angular.module('limerickApp', []).controller("limerickController", function($scope, $http, RequestService) {
 
-  this.getRhymes = function(word) {
-    RequestService.getRhymes(word, function(response) {
-      $scope.rhymes = response.data;
-      self.getNouns($scope.rhymes);
-    });
-  };
+    var self = this;
+    self.show = false;
 
-  self.getNouns = function(wordsArray) {
-    var nouns = [];
-    for (count=0; count<wordsArray.length-1; count++) {
-      if (wordsArray[count].tags[0] === 'n') {
-        nouns.push(wordsArray[count].word);
-      }
-    }
-    var rand = Math.floor(Math.random()*nouns.length);
-    self.noun = nouns[rand];
-    self.show = true;
-  };
+    self.animateLeft = function() {
+      var container = document.getElementById("main-container");
+      var left = parseInt(window.getComputedStyle(container).getPropertyValue("left"));
+      console.log(left);
+      container.style.left = left-100 + "vw";
+    };
 
+    this.buildLimerick = function(word, person) {
+        RequestService.getRhymes(word, function(response) {
+            $scope.rhymes = response.data;
+            self.getNouns($scope.rhymes, person);
+        });
+    };
+
+    self.getNouns = function(wordsArray, person) {
+        var nouns = [];
+        for (count = 0; count < wordsArray.length - 1; count++) {
+            if (wordsArray[count].tags[0] === 'n') {
+                nouns.push(wordsArray[count].word);
+            }
+        }
+        var rand = Math.floor(Math.random() * nouns.length);
+        self.noun = nouns[rand];
+        self.getArticle(person);
+    };
+
+    self.getArticle = function(person) {
+        if (person === 'man') {
+          self.article = 'He';
+        } else if (person === 'woman') {
+          self.article = 'She';
+        } else {
+          self.article = 'It';
+        }
+        self.show = true;
+    };
+
+    self.getNounRhyme = function() {
+
+    };
 
 });
